66c66b1254f2e5061e2f3a1dc2afb6de978cf9c9
Rick Weitzel <weitzel@umich.edu>
2017-01-18 11:09:41 -0500

    added comments to header

--- gapminder_analysis.R
+++ gapminder_analysis.R
@@ -1,3 +1,6 @@
+#notes from workshop
+#practicing using git commands with this
+
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "/Users/rick/Projects/R_stuff/gapminder-FiveYearData.csv")
 gapminder <- read.csv("gapminder-FiveYearData.csv")
 
