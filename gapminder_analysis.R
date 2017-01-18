47639b0beaba4aecbd27b529c887dd381be987c8
Rick Weitzel <weitzel@umich.edu>
2017-01-18 11:56:17 -0500

    comments change

--- gapminder_analysis.R
+++ gapminder_analysis.R
@@ -1,6 +1,6 @@
 #notes from workshop
 #practicing using git commands with this
-# Author: Rick Weitzel
+# Author: Rick We
 
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "/Users/rick/Projects/git_repos/swc_workshop/gapminder-FiveYearData.csv")
 gapminder <- read.csv("gapminder-FiveYearData.csv")
